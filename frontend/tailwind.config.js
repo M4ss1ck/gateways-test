8b68445c5a7cab423b318db7d5dc4c158485c7c4
M4ss1ck <4ndyraul@gmail.com>
2022-05-21 02:35:00 -0400

    change theme colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,10 +5,10 @@ module.exports = {
     extend: {
       colors: {
         primary: "#845EC2",
-        dark: "#4B4453",
+        dark: "#11416D", // #4B4453
         light: "#B0A8B9",
         alert: "#C34A36",
-        warning: "#FF8066",
+        warning: "#DCA11C", // #FF8066
       },
     },
   },
