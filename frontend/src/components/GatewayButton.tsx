140d11689539ec0ab82e14a9250386c181df4676
M4ss1ck <4ndyraul@gmail.com>
2022-05-22 12:26:51 -0400

    add: view raw output

--- frontend/src/components/GatewayButton.tsx
+++ frontend/src/components/GatewayButton.tsx
@@ -4,6 +4,7 @@ import DeviceList from "./DeviceList";
 
 const GatewayButton: React.FC<{ gateway: Gateway }> = ({ gateway }) => {
   const [showDetails, setShowDetails] = useState(false);
+  const [showRaw, setShowRaw] = useState(false);
   const [details, setDetails] = useState<Gateway | {}>({});
 
   const gatewayDetails = async (id: string) => {
@@ -26,23 +27,40 @@ const GatewayButton: React.FC<{ gateway: Gateway }> = ({ gateway }) => {
       ) : (
         Object.keys(details).length > 0 && (
           <>
-            <li className="relative flex flex-col items-start m-2 border rounded-lg border-warning">
+            <li className="relative flex flex-col items-start p-2 m-2 border rounded-lg border-warning">
               <button
                 onClick={() => setShowDetails(false)}
-                className="absolute top-0 right-0 px-2 translate-x-1/2 -translate-y-1/2 border rounded-md border-alert text-alert hover:text-light hover:bg-alert bg-dark"
+                className="absolute top-0 right-0 px-2 translate-x-1/2 -translate-y-1/2 border rounded-md border-alert text-alert hover:text-dark hover:bg-alert bg-dark"
               >
                 X
               </button>
-              {Object.entries(details).map(([name, value]) => {
-                return (
-                  name !== "peripherals" && (
-                    <Card key={name} name={name} data={value as string} />
-                  )
-                );
-              })}
-              {gateway.peripherals && gateway.id ? (
-                <DeviceList peripherals={gateway.peripherals} id={gateway.id} />
-              ) : null}
+              <button
+                onClick={() => setShowRaw(!showRaw)}
+                className="absolute top-0 px-2 -translate-y-1/2 border rounded-md right-8 border-warning text-warning hover:text-dark hover:bg-warning bg-dark"
+              >
+                {showRaw ? "Hide Raw" : "Raw"}
+              </button>
+              {showRaw ? (
+                <pre className="p-3 m-2 text-xs text-left text-light bg-opacity-30 bg-primary">
+                  {JSON.stringify(details, null, 2)}
+                </pre>
+              ) : (
+                <React.Fragment>
+                  {Object.entries(details).map(([name, value]) => {
+                    return (
+                      name !== "peripherals" && (
+                        <Card key={name} name={name} data={value as string} />
+                      )
+                    );
+                  })}
+                  {gateway.peripherals && gateway.id ? (
+                    <DeviceList
+                      peripherals={gateway.peripherals}
+                      id={gateway.id}
+                    />
+                  ) : null}
+                </React.Fragment>
+              )}
             </li>
           </>
         )
